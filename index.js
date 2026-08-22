609958f33754ef17441735702887c98176757db2
Paul <paullawal.ca@gmail.com>
2026-08-22 16:05:10 -0600

    I added the slider that says Bible.io Live everyday a blast of fun the whole way at the bottoom of the start up page, it slides which is pretty cool, Saturday, August 22, 2026

--- index.js
+++ index.js
@@ -99,3 +99,8 @@ allInputs.forEach (input => {
         }
     })
 })
+
+
+/** Function to make logos slide  */
+const logos = document.querySelector(".sliding-logos__track").cloneNode(true)
+document.querySelector(".sliding-logos").appendChild(logos)
