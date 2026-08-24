a70ef2f32b3ce743d3288aac380eecdf07a60d4a
Paul <paullawal.ca@gmail.com>
2026-08-24 12:46:00 -0600

    Still working on Bibletar Maker, Monday, August 24, 2026

--- shop.js
+++ shop.js
@@ -16,23 +16,34 @@ function wipeOutEntireScreen() {
     }
 }
 
-function drawSomething() {
+function bibleMaker_background () {
     // blue background
-
+    
     var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-
+    
     // 3. Add color stops
     // gradient.addColorStop(0, 'red');     // Start color (0%)
     // gradient.addColorStop(0.5, 'yellow'); // Middle color (50%)
     // gradient.addColorStop(1, 'blue');    // End color (100%)
-
-    gradient.addColorStop(0, 'red');     // Start color (0%)
-    gradient.addColorStop(0.5, 'yellow'); // Middle color (50%)
-    gradient.addColorStop(1, 'blue');    // End color (100%)
-
+    
+    gradient.addColorStop(0, 'rgb(0, 48, 138)');     // Start color (0%)
+    // gradient.addColorStop(0.5, 'yellow'); // Middle color (50%)
+    gradient.addColorStop(1, 'rgb(1, 152, 34)');    // End color (100%)
+    
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // black stroke
+    ctx.fillStyle = 'rgb(3, 3, 3)';
+    ctx.fillRect(320, 20, 870, 120);
+    ctx.fillStyle = 'rgb(3, 3, 3)';
+    ctx.fillRect(327, 7, 876, 126);
+    ctx.fillStyle = 'rgb(1, 55, 146)';
+    ctx.fillRect(330, 10, 870, 120);
+
+}
+
+function loadingBox () {
     // looading box
     ctx.fillStyle = 'rgba(255, 26, 104, 1)';
     ctx.fillRect(50 + box_x_pos, canvas.height - 100, 50, 50);
@@ -40,6 +51,14 @@ function drawSomething() {
     if (box_x_pos > canvas.width - 200) {
         box_x_pos = 0;
     }
+
+}
+
+function drawSomething() {
+
+    bibleMaker_background();
+    loadingBox();
+
 
     if(front_page_text.length < 20) {
         front_page_text.push(0);
