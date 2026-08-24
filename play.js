ec65685c3e5b41f37cb7c88fe4f90dcf82434e43
Paul <paullawal.ca@gmail.com>
2026-08-23 19:49:49 -0600

    Fixed copy and pasting bug in play section of Bible.io, Sunday, August 23, 2026

--- play.js
+++ play.js
@@ -1,19 +1,43 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+const big_text = document.getElementById('big-text');
 
 var box_x_pos = 1;
+var gameOn = false;
+/* Being able to copy and paste text by using HTML is super important, especially when the text is super long,
+that's one thing I like about HTML. As well as it's the core foundation to building websites. */
+var play_front_page_text = [];
 
-function wipeEntireScreen() {
+function wipeOutEntireScreen() {
     ctx.clearRect(0,0, canvas.width, canvas.height);
+    if (gameOn != false) {
+        // When I keep erasing and rewriting it, I can't copy and paste the text
+        big_text.innerText = '';
+    }
 }
 
 function drawSomething() {
+    // looading box
     ctx.fillStyle = 'rgba(255, 26, 104, 1)';
     ctx.fillRect(50 + box_x_pos, canvas.height - 100, 50, 50);
     box_x_pos += 3;
     if (box_x_pos > canvas.width - 200) {
         box_x_pos = 0;
     }
+
+    if(play_front_page_text.length < 20) {
+        play_front_page_text.push(0);
+    }
+
+    // Bible.io Live Text
+    if (play_front_page_text[0] == 0) {
+        // When I keep erasing and rewriting it, I can't copy and paste the text, that's why I'm doing this
+        big_text.innerText = 'Bible.io Live';
+        play_front_page_text[0] +=1;
+    } else {
+        // do nothing
+    }
+    // console.log(big_text.innerText);
 }
 
 function resizeCanvas() {
@@ -29,7 +53,7 @@ function resizeCanvas() {
 
 function gameLoop() {
     /**Wiping the entire screen clear is important before drawing your next batch */
-    wipeEntireScreen();
+    wipeOutEntireScreen();
     drawSomething();
     /** All that requestAnimationFrame does it create a forever loop that can help me make
      * games or animations also, you can't control the fps it specifically hooked to match your
