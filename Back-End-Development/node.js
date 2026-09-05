58b40b5f74c0f49e79893cdb25c86c6c4d5379d5
Paul <paullawal.ca@gmail.com>
2026-09-05 16:37:02 -0600

    add more comments and lengthy discussion just talking about back-end development, responsibilities, and money, Saturday, September 5, 2026

---
+++ Back-End-Development/node.js
@@ -0,0 +1,46 @@
+/** Well, I'm highly interested in the idea of Full-Stack Javascript, if you don't know what full-stack
+ * javascript is, it is essentially where you use Javascript both for front-end development as well
+ * as back-end development. For JavaScript Back-end frameworks, you will be using Node.js
+ * 
+ * I guess some important things to understand as a Full-Stack Developer, especially when talking about the
+ * back-end, is cloud servers and web servers. Some important things to know is, where are Cloud Servers and
+ * Web Servers located. The answer is in Data Centers. Not just Data Centers but in other unknown places.
+ * We also have underwater cables that help countries and the entire world stay connected as well as 
+ * transfer data quickly and efficiently. Such as messages, photos, audio, and much more.
+ * 
+ * Another important thing to note is that a websites' name or domain name is not understood by computers
+ * but is made for humans to understand be able to find websites much easier. A websites name uses letters
+ * that is used to represent an IP Address. These IP Addresses are stored on Web Servers, and when
+ * humans type out these websites and press enter, it sends a request to these servers which in turn send
+ * out other requests to other servers and send out a response to the user who searched for it. Supplying
+ * them with the websites data such as photos, text, and etc.
+ * 
+ * Looks let's be honest with ourselves, Back-End Development is problematic in so many ways because I mean
+ * first of all it is server-side scripting. Client Side Scripting is easy because it is run on the 
+ * user's computer and you don't even need Wi-Fi for it. Because Server Side Scripting is more problematic
+ * because you have to send requests to servers. The problem is, do you even have a server. It's one thing
+ * to build a website and deploy code to it, which costs some money, although there are definiitely free
+ * plans which aren't the best because your domain name isn't fully customizable and looks less 
+ * professionable. But do you want to build your own server, and store it in your closet. People who 
+ * build their own servers face many difficulties such as security. If you don't set things up correctly
+ * you might actually end up displaying your home's IP Address out to the public, which is enough for
+ * hackers to use to take down your entire household's Wi-Fi. Unless you are tech savvy enough to do this,
+ * don't do it!! You can choose to buy your own server meaning that you have to maintain everything
+ * yourself, or  you can rent servers and the company that you rent it from also bears some of the
+ * responsibility. Although there are different plans, where you can get shared hosting, which is
+ * less predictable and you share a server with multiple other people. The better your hosting, the
+ * more money it costs, but the more reliable it is. It's one thing to know how to code for back-end
+ * it is another completely different thing to actually have a big enough budget to pay for the servers
+ * you are going to use, which is a huge problem that Bible.io is facing. Storing everyone's account,
+ * login information and etc is painful, because that will costs a lot of money. Maybe get some functions
+ * online is fine, and it won't cost too much depending on how many people actually play or use Bible.io
+ * because as you already know the more traffic that your website receives, the more money you have
+ * to pay to keep your website up due to network bandwidth and more server power.
+ * 
+ * And if let's just say theoretically Bible.io wants to make some money at least so that they can pay
+ * for the servers, where will that money come from. Or we supposed to hook Ads up on our website, just
+ * so that we can pay for the servers. Ads can be super annoying websites. Or we will we fundraise
+ * just through website traffic, which probably we won't be making even enough money to pay for all
+ * the online functions. It's one thing to code, it's another thing to have the money and the cash.
+ * Because let's be honest with ourselves. Nothing goes for nothing. And there ain't NOTHIN' that's free!!
+ */
